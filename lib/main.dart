07b2eba87978946ac3dca781816e38b91102a4c4
yuosif1286 <84059356+yuosif1286@users.noreply.github.com>
2024-07-25 04:16:02 +0300

    new app

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,6 @@ class Tune extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
     );
   }
 }
