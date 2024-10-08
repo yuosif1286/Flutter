2af9614270ef245e6badc8ea7bcd2e21a5418e15
yuosif1286 <84059356+yuosif1286@users.noreply.github.com>
2024-10-09 02:22:45 +0300

    sample with using media query to get width and height screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,51 @@
-import 'package:first123project/views/home_view.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const News());
+  runApp(const MyHomePage());
 }
 
-class News extends StatelessWidget {
-  const News({super.key});
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeView(),
+    // MediaQuery for getting screen dimensions
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    return MaterialApp(
+      title: 'My Home Page',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Home Page'),
+          centerTitle: true,
+        ),
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: screenWidth * 0.25, // 20% of screen width
+              height: 200, // 20% of screen height
+              color: Colors.red,
+            ),
+            Container(
+              width: screenWidth * 0.25,
+              height: 200,
+              color: Colors.blue,
+            ),
+            Container(
+              width: screenWidth * 0.25,
+              height: 200,
+              color: Colors.green,
+            ),
+            Container(
+              width: screenWidth * 0.25,
+              height: 200,
+              color: Colors.yellowAccent,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
