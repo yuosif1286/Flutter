b350d0333901af22374f79229e1d20aceb95f452
yuosif1286 <84059356+yuosif1286@users.noreply.github.com>
2024-10-13 23:54:59 +0300

    finished

--- lib/widgets/NavLayout.dart
+++ lib/widgets/NavLayout.dart
@@ -10,12 +10,35 @@ class NavLayout extends StatefulWidget {
 class _NavLayoutState extends State<NavLayout> {
   @override
   Widget build(BuildContext context) {
-    return const CupertinoPageScaffold(
+    return CupertinoPageScaffold(
         child: Center(
-            child: CupertinoActivityIndicator(
-      color: CupertinoColors.activeOrange,
-      radius: 50,
-      
+            child: CupertinoButton(
+      child: const Text('Cupertino Alert Dialog'),
+      onPressed: () {
+        showCupertinoDialog<void>(
+            context: context,
+            builder: (BuildContext context) => CupertinoAlertDialog(
+                    title: const Text('This Title Dialog'),
+                    content: const Text('This is Containt Dialog ?'),
+                    actions: <CupertinoDialogAction>[
+                      CupertinoDialogAction(
+                        child: const Text('Cancel'),
+                        textStyle:
+                            const TextStyle(color: CupertinoColors.systemRed),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                      CupertinoDialogAction(
+                        child: const Text('Confirm'),
+                        textStyle:
+                            const TextStyle(color: CupertinoColors.activeGreen),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ]));
+      },
     )));
   }
 }
