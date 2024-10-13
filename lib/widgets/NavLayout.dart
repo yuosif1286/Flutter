d2d108cce1265d4dd5d9a3f37560fff3a68bae0d
yuosif1286 <84059356+yuosif1286@users.noreply.github.com>
2024-10-13 23:41:01 +0300

    finished

--- lib/widgets/NavLayout.dart
+++ lib/widgets/NavLayout.dart
@@ -10,31 +10,12 @@ class NavLayout extends StatefulWidget {
 class _NavLayoutState extends State<NavLayout> {
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
+    return const CupertinoPageScaffold(
         child: Center(
-      child: CupertinoButton(
-        child: Text('Cupertino Action Sheet'),
-        onPressed: () {
-          showCupertinoModalPopup(
-              context: context,
-              builder: (BuildContext context) => CupertinoActionSheet(
-                    title: const Text('Flutter Map'),
-                    message: const Text('Your Message'),
-                    actions: <CupertinoActionSheetAction>[
-                      CupertinoActionSheetAction(
-                          child: const Text('do some thing'),
-                          onPressed: () {
-                            Navigator.pop(context);
-                          }),
-                      CupertinoActionSheetAction(
-                          child: const Text('do some thing else'),
-                          onPressed: () {
-                            Navigator.pop(context);
-                          })
-                    ],
-                  ));
-        },
-      ),
-    ));
+            child: CupertinoActivityIndicator(
+      color: CupertinoColors.activeOrange,
+      radius: 50,
+      
+    )));
   }
 }
