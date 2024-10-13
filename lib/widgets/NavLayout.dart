2013c8d9432d38e4972e5fe0c90f8909fb868e6b
yuosif1286 <84059356+yuosif1286@users.noreply.github.com>
2024-10-14 02:40:10 +0300

    finished

--- lib/widgets/NavLayout.dart
+++ lib/widgets/NavLayout.dart
@@ -12,20 +12,29 @@ class _NavLayoutState extends State<NavLayout> {
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
         child: Center(
-            child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        const CupertinoButton.filled(onPressed: null, child: Text('Add Test')),
-        const SizedBox(
-          height: 30,
+      child: SizedBox(
+        height: 100,
+        width: 100,
+        child: CupertinoContextMenu(
+          actions: <CupertinoContextMenuAction>[
+            CupertinoContextMenuAction(
+              child: const Text("One"),
+              onPressed: () {
+                Navigator.pop(context); // Pop the context menu
+              },
+            ),
+            CupertinoContextMenuAction(
+              child: const Text("Two"),
+              onPressed: () {
+                Navigator.pop(context); // Pop the context menu
+              },
+            ),
+          ],
+          child: Image.network(
+            "https://images.pexels.com/photos/213780/pexels-photo-213780.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+          ),
         ),
-        CupertinoButton.filled(
-            child: const Text('Edit Test'), onPressed: () {}),
-        const SizedBox(
-          height: 30,
-        ),
-        const CupertinoButton(onPressed: null, child: Text('Delete Test')),
-      ],
-    )));
+      ),
+    ));
   }
 }
