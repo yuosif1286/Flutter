c37f1dde939a7811e25fe1e5711424d20ee5f690
yuosif1286 <84059356+yuosif1286@users.noreply.github.com>
2024-10-15 00:10:08 +0300

    finished

--- lib/widgets/NavLayout.dart
+++ lib/widgets/NavLayout.dart
@@ -9,37 +9,24 @@ class NavLayout extends StatefulWidget {
 }
 
 class _NavLayoutState extends State<NavLayout> {
-  int _selectedValue = 0;
-
   @override
   Widget build(BuildContext context) {
     return Center(
       child: CupertinoButton.filled(
-        child: Text('value = $_selectedValue'),
-        onPressed: () => showCupertinoModalPopup(
-          context: context,
-          builder: (_) => SizedBox(
-            width: double.infinity,
-            height: 250,
-            child: CupertinoPicker(
-              itemExtent: 30,
-              backgroundColor: Colors.blueAccent.withOpacity(0.5),
-              scrollController: FixedExtentScrollController(initialItem: 0),
-              onSelectedItemChanged: (int newValue) {
-                setState(() {
-                  _selectedValue = newValue;
-                });
-              },
-              children: const [
-                Text('0'),
-                Text('1'),
-                Text('2'),
-                Text('3'),
-              ],
-            ),
-          ),
-        ),
-      ),
+          child: const Text('click me'),
+          onPressed: () => showCupertinoModalPopup(
+              context: context,
+              builder: (_) => Container(
+                    transformAlignment: Alignment.center,
+                    color: Colors.white,
+                    height: 400,
+                    width: double.infinity,
+                    child: Center(
+                      child: CupertinoButton(
+                          child: const Text('Close'),
+                          onPressed: () => {Navigator.of(context).pop()}),
+                    ),
+                  ))),
     );
   }
 }
