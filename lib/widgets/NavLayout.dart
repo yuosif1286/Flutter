90cc5fe54e2a18b823f3844d5eb8c38e8e1794c0
yuosif1286 <84059356+yuosif1286@users.noreply.github.com>
2024-10-16 04:37:29 +0300

    finished

--- lib/widgets/NavLayout.dart
+++ lib/widgets/NavLayout.dart
@@ -9,64 +9,27 @@ class NavLayout extends StatefulWidget {
 }
 
 class _NavLayoutState extends State<NavLayout> {
-  String? _text;
+  double _value = 1;
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return SizedBox(
+      width: double.infinity,
       child: Column(
         children: [
           const SizedBox(height: 50),
-          CupertinoSegmentedControl<String>(
-            children: {
-              'Youtube': Container(
-                color: _text == 'Youtube'
-                    ? Colors.greenAccent.withOpacity(0.3)
-                    : Colors.white,
-                width: double.infinity,
-                height: double.infinity,
-                padding: const EdgeInsets.all(8),
-                child: const Text(
-                  'Youtube',
-                  style: TextStyle(fontSize: 20),
-                ),
-              ),
-              'Flutter': Container(
-                color: _text == 'Flutter'
-                    ? Colors.greenAccent.withOpacity(0.3)
-                    : Colors.white,
-                width: double.infinity,
-                height: double.infinity,
-                padding: const EdgeInsets.all(8),
-                child: const Text(
-                  'Flutter',
-                  style: TextStyle(fontSize: 20),
-                ),
-              ),
-              'Google': Container(
-                color: _text == 'Google'
-                    ? Colors.greenAccent.withOpacity(0.3)
-                    : Colors.white,
-                width: double.infinity,
-                padding: const EdgeInsets.all(8),
-                height: double.infinity,
-                child: const Text(
-                  'Google',
-                  style: TextStyle(fontSize: 20),
-                ),
-              ),
-            },
-            onValueChanged: (newValue) {
+          Text('$_value'),
+          const SizedBox(height: 50),
+          CupertinoSlider(
+            value: _value,
+            min: 0,
+            max: 10,
+            divisions: 10,
+            onChanged: (newValue) {
               setState(() {
-                _text = newValue;
+                _value = newValue;
               });
             },
-          ),
-          const SizedBox(height: 50),
-          if (_text != null)
-            Text(
-              _text!,
-              style: const TextStyle(fontSize: 50),
-            ),
+          )
         ],
       ),
     );
